597a026ab2ec3c621d5fa0937eae7dd2c0dbc6d5
Naiim <saidi.naim123@gmail.com>
2023-10-16 20:56:00 +0200

    update file

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/HangMan/',
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+  },
 });
